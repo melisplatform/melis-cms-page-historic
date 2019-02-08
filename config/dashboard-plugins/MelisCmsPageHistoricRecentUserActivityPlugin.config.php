6d4453c011dc1d8ca9ec60f33592c83445ffff77
Cris <crismakabenta@gmail.com>
2019-02-08 17:02:38 +0800

    Updated interface to adopt the dashboard plugin rights

--- config/dashboard-plugins/MelisCmsPageHistoricRecentUserActivityPlugin.config.php
+++ config/dashboard-plugins/MelisCmsPageHistoricRecentUserActivityPlugin.config.php
@@ -1,6 +1,26 @@
 <?php 
     return array(
         'plugins' => array(
+            'meliscore' => [
+                'interface' => [
+                    'melis_dashboardplugin' => [
+                        'conf' => [
+                            'dashboard_plugin' => true
+                        ],
+                        'interface' => [
+                            'melisdashboardplugin_section' => [
+                                'interface' => [
+                                    'MelisCmsPageHistoricRecentUserActivityPlugin' => [
+                                        'conf' => [
+                                            'type' => '/meliscmspagehistoric/interface/MelisCmsPageHistoricRecentUserActivityPlugin'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+            ],
             'meliscmspagehistoric' => array(
                 'ressources' => array(
                     'css' => array(
@@ -8,27 +28,31 @@
                     'js' => array(
                     )
                 ),
-                'dashboard_plugins' => array(
+                'interface' => array(
                     'MelisCmsPageHistoricRecentUserActivityPlugin' => array(
-                        'plugin_id' => 'recentActivity',
-                        'name' => 'tr_melispagehistoric_dashboard_recent_activity_Pages',
-                        'description' => 'tr_melispagehistoric_dashboard_recent_activity_Pages description',
-                        'icon' => 'fa fa-users',
-                        'thumbnail' => '/MelisCmsPageHistoric/plugins/images/MelisCmsPageHistoricRecentUserActivityPlugin.jpg',
-                        'jscallback' => '',
-                        'max_lines' => 8,
-                        'height' => 4,
-                        
-                        'interface' => array(
-                            'meliscmspagehistoric_events' => array(
-                                'forward' => array(
-                                    'module' => 'MelisCmsPageHistoric',
-                                    'plugin' => 'MelisCmsPageHistoricRecentUserActivityPlugin',
-                                    'function' => 'recentActivityPages',
-                                    'jscallback' => '',
-                                    'jsdatas' => array()
-                                ),
-                            ),
+                        'conf' => array(
+                            'name' => 'MelisCmsPageHistoricRecentUserActivityPlugin',
+                            'melisKey' => 'MelisCmsPageHistoricRecentUserActivityPlugin'
+                        ),
+                        'datas' => array(
+                            'plugin_id' => 'recentActivity',
+                            'name' => 'tr_melispagehistoric_dashboard_recent_activity_Pages',
+                            'description' => 'tr_melispagehistoric_dashboard_recent_activity_Pages',
+                            'icon' => 'fa fa-users',
+                            'thumbnail' => '/MelisCmsPageHistoric/plugins/images/MelisCmsPageHistoricRecentUserActivityPlugin.jpg',
+                            'jscallback' => '',
+                            'max_lines' => 8,
+                            'height' => 4,
+                            'width' => 0,
+                            'x-axis' => 0,
+                            'y-axis' => 0,
+                        ),
+                        'forward' => array(
+                            'module' => 'MelisCmsPageHistoric',
+                            'plugin' => 'MelisCmsPageHistoricRecentUserActivityPlugin',
+                            'function' => 'recentActivityPages',
+                            'jscallback' => '',
+                            'jsdatas' => array()
                         ),
                     ),
                 ),
