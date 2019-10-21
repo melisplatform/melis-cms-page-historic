2583bac2bc4d82b4bf3da941e51a0303eea2d049
ksuson <ksuson@melistechnology.com>
2019-10-21 17:59:54 +0800

    Refactored with the correct usage of dateramgepicker, & code cleanup

--- public/js/melispagehistoric.js
+++ public/js/melispagehistoric.js
@@ -1,180 +1,158 @@
-$(function(){
-	// cache body
-	var $body                   = $("body"),
+$(function () {
+    // cache body
+    var $body = $("body"),
         tableId,
-        historicDateFilterStart = "",
-        historicDateFilterEnd   = "",
         historicBackOfficeUsers = [],
-        selectedUser            = "";
-	
-        window.initHistoric = function(data, tblSettings) {
-            // add events here if you want to do something when initializing page historic
-            getBackofficeUsers();
-            
-            // remove the sort icon in the table head
-            $(document).on("init.dt", function(e, settings) {
-                var thUserId = $(".tableHistoric thead tr").find("th:nth-child(1)");
+        selectedUser = "";
 
-                    thUserId.removeClass("sorting_asc");
-                    initSearchUserAutoCompleteInput();
-                    initDateRangePickerFilter();
+    window.initHistoric = function (data, tblSettings) {
+        // add events here if you want to do something when initializing page historic
+        getBackofficeUsers();
+
+        // remove the sort icon in the table head
+        $(document).on("init.dt", function (e, settings) {
+            var thUserId = $(".tableHistoric thead tr").find("th:nth-child(1)");
+
+            thUserId.removeClass("sorting_asc");
+            initSearchUserAutoCompleteInput();
+            initDateRangePickerFilter();
+        });
+
+        // get the current page ID
+        var pageId = $("#" + tblSettings.sTableId).data("pagenumber");
+
+        // pass what page ID to be used when displaying page historic
+        data.pageId = pageId;
+
+        data.user_name = selectedUser;
+        initSelectedActionData(data);
+
+        /** Get current datepicker >> check for start & end dates >> include dates in AJaX request */
+        var pckr = $body.find("#" + tblSettings.sTableId + "_wrapper .melisCmsPageHistoricDatePicker");
+        if (pckr.length > 0) {
+            pckr = pckr.data('daterangepicker');
+            if (pckr.startDate !== null && pckr.endDate !== null) {
+                data.startDate = pckr.startDate.format(melisDateFormat);
+                data.endDate = pckr.endDate.format(melisDateFormat);
+            }
+        }
+    };
+
+    //cancel daterange picker
+    $body.on('cancel.daterangepicker', '#' + activeTabId + ' .tableHistoric', function (ev, picker) {
+        var $this = $(this);
+
+        $this.find('input[name="datefilter"]').val('');
+    });
+
+    //refresh table content after selecting an action
+    $body.on('change', '.melisCmsPageHistoricSelectAction', function () {
+        var $this = $(this);
+
+        tableId = $this.closest('.filter-bar').siblings('.bottom').find('table').attr('id');
+        $("#" + tableId).DataTable().ajax.reload();
+    });
+
+    // Refresh table content when date(range) is selected
+    $body.on('apply.daterangepicker', ".melisCmsPageHistoricDatePicker", function () {
+        var $this = $(this);
+
+        var tableId = $this.closest('.filter-bar').siblings('.bottom').find('table').attr('id');
+        $("#" + tableId).DataTable().ajax.reload();
+    });
+
+    //initialize autocomplete input for BO users
+    window.initSearchUserAutoCompleteInput = function () {
+        $body.find('.melisCmsPageHistoricSearchUserText').autocomplete({
+            source: historicBackOfficeUsers,
+            select: function (event, ui) {
+                //on select get the value
+                selectedUser = ui.item.value;
+                tableId = $(this).closest('.filter-bar').siblings('.bottom').find('table').attr('id');
+                $("#" + tableId).DataTable().ajax.reload();
+            }
+        });
+    }
+
+    //get all BO users present in the pagehistoric database
+    window.getBackofficeUsers = function () {
+        historicBackOfficeUsers = [];
+        $.ajax({
+            type: 'POST',
+            url: '/melis/MelisCmsPageHistoric/PageHistoric/getBackOfficeUsers',
+        }).done(function (data) {
+            $.each(data.users, function (key, value) {
+                historicBackOfficeUsers.push(value.fullname);
             });
-            
-            // get the current page ID
-            var pageId = $("#" + tblSettings.sTableId ).data("pagenumber");
-            
-                // pass what page ID to be used when displaying page historic
-                data.pageId = pageId;
-
-                data.user_name = selectedUser;
-                initSelectedActionData(data);
-
-                if ( historicDateFilterStart != "" ) {
-                    data.startDate = historicDateFilterStart;
-                }
-
-                if ( historicDateFilterEnd != "" ) {
-                    data.endDate = historicDateFilterEnd;
-                }
-
-                //$("#tableHistoricPageId"+pageId).DataTable();
-                //$body.find(".melis-refreshPageTable").trigger("click");
-        };
-
-        // refreshPageTable
-        // id attribute selector
-        // $("div[id="+activeTabId+"]")
-
-        //$body.on("click", ".nav-tabs li a.history", function() {
-            //melisCms.refreshPageTable();
-            //$($.fn.dataTable.tables( true ) ).css('width', '100%');
-            //$($.fn.dataTable.tables( true ) ).DataTable().columns.adjust().draw();
-            //$body.find(".melis-refreshPageTable").click();
-        //});
-
-        //cancel daterange picker
-        $body.on('cancel.daterangepicker', '#'+activeTabId+' .tableHistoric', function(ev, picker) {
-            var $this = $(this);
-
-                $this.find('input[name="datefilter"]').val('');
+        }).fail(function () {
+            alert(translations.tr_meliscore_error_message);
         });
+    }
 
-        //refresh table content after selecting an action
-        $body.on('change', '.melisCmsPageHistoricSelectAction',function(){
-            var $this = $(this);
-                
-                tableId = $this.closest('.filter-bar').siblings('.bottom').find('table').attr('id');
-                $("#"+tableId).DataTable().ajax.reload();
-        });
+    //initialize date range picker
+    window.initDateRangePickerFilter = function () {
+        var sToday = translations.tr_meliscore_datepicker_today,
+            sYesterday = translations.tr_meliscore_datepicker_yesterday,
+            sLast7Days = translations.tr_meliscore_datepicker_last_7_days,
+            sLast30Days = translations.tr_meliscore_datepicker_last_30_days,
+            sThisMonth = translations.tr_meliscore_datepicker_this_month,
+            sLastMonth = translations.tr_meliscore_datepicker_last_month;
 
-        //refresh table content when date is selecterd
-        $body.on('apply.daterangepicker', ".melisCmsPageHistoricDatePicker", function(){
-            var $this = $(this);
+        function callback(start, end) {
+            dStartDate = start.format(melisDateFormat);
+            dEndDate = end.format(melisDateFormat);
 
-                tableId = $this.closest('.filter-bar').siblings('.bottom').find('table').attr('id');
-                $("#"+tableId).DataTable().ajax.reload();
-        });
+            //default display upon initialization of date picker
+            var icon = '<i class="glyphicon glyphicon-calendar fa fa-calendar"></i>';
 
-        //initialize autocomplete input for BO users
-        window.initSearchUserAutoCompleteInput = function(){
-            $body.find('.melisCmsPageHistoricSearchUserText').autocomplete({
-                source: historicBackOfficeUsers,
-                select: function (event, ui) {
-                    //on select get the value
-                    selectedUser = ui.item.value;
-                    tableId = $(this).closest('.filter-bar').siblings('.bottom').find('table').attr('id');
-                    $("#"+tableId).DataTable().ajax.reload();
-                }
-            });
+            if (dStartDate == "") {
+                $body.find(".melisCmsPageHistoricDatePicker .dt_dateInfo").html(translations.tr_meliscore_datepicker_select_date + ' ' + icon + ' <strong class="caret"></strong>');
+
+            } else {
+                $body.find(".melisCmsPageHistoricDatePicker .dt_dateInfo").html(translations.tr_meliscore_datepicker_select_date + ' ' + icon + ' ' + dStartDate + ' - ' + dEndDate + ' <b class="caret"></b>');
+            }
         }
 
-        //get all BO users present in the pagehistoric database
-        window.getBackofficeUsers = function (){
-            historicBackOfficeUsers = [];
-            $.ajax({
-                type        : 'POST',
-                url         : '/melis/MelisCmsPageHistoric/PageHistoric/getBackOfficeUsers',
-            }).done(function(data){
-                $.each(data.users, function(key, value) {
-                    historicBackOfficeUsers.push(value.fullname);
-                });
-            }).fail(function() {
-                alert( translations.tr_meliscore_error_message );
-            });
+        var rangeStringParam = {};
+
+        rangeStringParam[sToday] = [moment(), moment()];
+        rangeStringParam[sYesterday] = [moment().subtract(1, 'days'), moment().subtract(1, 'days')];
+        rangeStringParam[sLast7Days] = [moment().subtract(6, 'days'), moment()];
+        rangeStringParam[sLast30Days] = [moment().subtract(29, 'days'), moment()];
+        rangeStringParam[sThisMonth] = [moment().startOf('month'), moment().endOf('month')];
+        rangeStringParam[sLastMonth] = [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')];
+
+        $body.find(".melisCmsPageHistoricDatePicker").daterangepicker({
+            locale: {
+                format: melisDateFormat,
+                applyLabel: translations.tr_meliscore_datepicker_apply,
+                cancelLabel: translations.tr_meliscore_datepicker_cancel,
+                customRangeLabel: translations.tr_meliscore_datepicker_custom_range,
+            },
+            ranges: rangeStringParam,
+        }, callback);
+    }
+
+    //this will get the value of the select and add it into the data so it will be passed in the backend
+    window.initSelectedActionData = function (data) {
+        var actionField = $('#' + tableId).closest('.bottom').siblings('.filter-bar').find('.melisCmsPageHistoricSelectAction');
+
+        if (actionField.length && actionField.val() != "") {
+            data.action = $('#' + tableId).closest('.bottom').siblings('.filter-bar').find('.melisCmsPageHistoricSelectAction').val();
         }
+    }
 
-        //initialize date range picker
-        window.initDateRangePickerFilter = function() {
-            historicDateFilterStart = moment().format(melisDateFormat);
-            historicDateFilterEnd   = historicDateFilterStart;
+    //open historic event
+    $body.on("click", '.melis-openrecenthistoric', function () {
+        var $this = $(this),
+            data = $this.data();
 
-            var sToday      = translations.tr_meliscore_datepicker_today,
-                sYesterday  = translations.tr_meliscore_datepicker_yesterday,
-                sLast7Days  = translations.tr_meliscore_datepicker_last_7_days,
-                sLast30Days = translations.tr_meliscore_datepicker_last_30_days,
-                sThisMonth  = translations.tr_meliscore_datepicker_this_month,
-                sLastMonth  = translations.tr_meliscore_datepicker_last_month;
+        //OPEN HISTORIC FROM DASHBOARD WIDGET
+        melisHelper.tabOpen(data.pageTitle, data.pageIcon, data.zoneId, data.melisKey, {idPage: data.pageId});
+    });
 
-                function callback(start, end) {
-                    dStartDate = start.format(melisDateFormat);
-                    dEndDate   = end.format(melisDateFormat);
-
-                    //default display upon initialization of date picker
-                    var icon = '<i class="glyphicon glyphicon-calendar fa fa-calendar"></i>';
-
-                    if (dStartDate == "") {
-                        $body.find(".melisCmsPageHistoricDatePicker .dt_dateInfo").html(translations.tr_meliscore_datepicker_select_date + ' ' + icon + ' <strong class="caret"></strong>');
-
-                    } else {
-                        $body.find(".melisCmsPageHistoricDatePicker .dt_dateInfo").html(translations.tr_meliscore_datepicker_select_date + ' ' + icon + ' ' + dStartDate + ' - ' + dEndDate + ' <b class="caret"></b>');
-                    }
-
-                    historicDateFilterStart = dStartDate;
-                    historicDateFilterEnd = dEndDate;
-
-                    //$("#"+tableId).DataTable().ajax.reload();
-                }
-
-            var rangeStringParam = {};
-
-                rangeStringParam[sToday]        = [moment(), moment()];
-                rangeStringParam[sYesterday]    = [moment().subtract(1, 'days'), moment().subtract(1, 'days')];
-                rangeStringParam[sLast7Days]    = [moment().subtract(6, 'days'), moment()];
-                rangeStringParam[sLast30Days]   = [moment().subtract(29, 'days'), moment()];
-                rangeStringParam[sThisMonth]    = [moment().startOf('month'), moment().endOf('month')];
-                rangeStringParam[sLastMonth]    = [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')];
-
-                $body.find(".melisCmsPageHistoricDatePicker").daterangepicker({
-                    locale : {
-                        format: melisDateFormat,
-                        applyLabel: translations.tr_meliscore_datepicker_apply,
-                        cancelLabel: translations.tr_meliscore_datepicker_cancel,
-                        customRangeLabel: translations.tr_meliscore_datepicker_custom_range,
-                    },
-                    ranges: rangeStringParam,
-                }, callback);
-        }
-
-        //this will get the value of the select and add it into the data so it will be passed in the backend
-        window.initSelectedActionData = function(data){
-            var actionField = $('#'+tableId).closest('.bottom').siblings('.filter-bar').find('.melisCmsPageHistoricSelectAction');
-
-                if ( actionField.length && actionField.val() != "" ) {
-                    data.action = $('#'+tableId).closest('.bottom').siblings('.filter-bar').find('.melisCmsPageHistoricSelectAction').val();
-                }
-        }
-        
-        //open historic event
-        $body.on("click", '.melis-openrecenthistoric', function(){
-            var $this   = $(this),
-                data    = $this.data();
-
-                //OPEN HISTORIC FROM DASHBOARD WIDGET
-                melisHelper.tabOpen( data.pageTitle, data.pageIcon, data.zoneId, data.melisKey,  { idPage: data.pageId } );
-        });
-
-        // paginate dataTables data
-        window.paginateDataTables = function() {
-            melisCore.paginateDataTables();
-        }
+    // paginate dataTables data
+    window.paginateDataTables = function () {
+        melisCore.paginateDataTables();
+    }
 });
