fc7687c7f96541c1bc11852dcece9f5d7476defe
unknown <crismakabenta@gmail.com>
2020-04-14 12:53:18 +0800

    Update module.config

--- config/module.config.php
+++ config/module.config.php
@@ -42,9 +42,6 @@ return [
             ],            
         ],
     ],
-    'translator' => [
-    	'locale' => 'en_EN',
-	],
     'service_manager' => [
         'aliases' => [
             'MelisPageHistoricTable' => \MelisCmsPageHistoric\Model\Tables\MelisPageHistoricTable::class,
@@ -61,8 +58,6 @@ return [
         ]
     ],
     'view_manager' => [
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
         'template_map' => [
             // Dashboard plugin templates
